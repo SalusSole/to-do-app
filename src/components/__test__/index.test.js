230cabeb0796f6c714ef5dd64dd23239421c122c
Salvador Solis <ch1.yomero.ss@gmail.com>
2021-01-30 06:37:08 -0600

    cambios en test

--- src/components/__test__/index.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../../App';
-
-test('unit testing', () => {
-  render(<App />);
-  const title = screen.getByText(/Task App/i);
-  expect(title).toBeInTheDocument();
-});
